52c94eec545f00548719ebd3f2ab0e15b610e38b
bigliweijie <1771662778@qq.com>
2023-10-09 17:02:12 +0800

    lwj

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -3,38 +3,39 @@ import homeBottomNav from './icon/homeBottomNav.png'
 import mineBottomNav from './icon/mineBottomNav.png'
 
 export default class BottomNav extends Component {
-  BottomNavStateSwichTo1=()=>{
+  BottomNavStateSwichTo1 = () => {
     this.props.BottomNavStateSwich(1)
   }
-  BottomNavStateSwichTo0=()=>{
+  BottomNavStateSwichTo0 = () => {
     this.props.BottomNavStateSwich(0)
   }
   render() {
-    const ssBottomNavBox={
+    const ssBottomNavBox = {
       display: 'flex',
-      justifyContent:'space-around',
+      justifyContent: 'space-around',
+      backgroundColor: '#FFF',
       position: "absolute",
       bottom: "0px",
       height: '8vh',
       width: '100%',
     }
-    const ssBottomNavitem={
+    const ssBottomNavitem = {
       display: 'flex',
       position: "relative",
       height: '8vh',
       width: '20%',
-      justifyContent:'center',
+      justifyContent: 'center',
       flexDirection: 'column',
       alignItems: 'center',
     }
     return (
       <div style={ssBottomNavBox}>
-        <div onClick={()=>{this.BottomNavStateSwichTo0()}} style={ssBottomNavitem}>
-          <img  src={ homeBottomNav } alt='主页' />
+        <div onClick={() => { this.BottomNavStateSwichTo0() }} style={ssBottomNavitem}>
+          <img src={homeBottomNav} alt='主页' />
           <div>主页</div>
         </div>
-        <div onClick={()=>{this.BottomNavStateSwichTo1()}} style={ssBottomNavitem}>
-          <img src={ mineBottomNav } alt='我的' />
+        <div onClick={() => { this.BottomNavStateSwichTo1() }} style={ssBottomNavitem}>
+          <img src={mineBottomNav} alt='我的' />
           <div>我的</div>
         </div>
       </div>
