91dc1330a9267e507038e90607937437daf6f9ba
Piu-Lin <55540481+Piu-Lin@users.noreply.github.com>
2023-10-09 09:49:12 +0800

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+  state = {
+    /** 乡村资讯 */
+    villageInformation: [{
+      /** 小图标 */
+      miniCon:"",
+      /** 标题 */
+      informationTitle:"",
+      /** 发布时间 */
+      pushTime:"",
+    }],
+  }
   render() {
     return (
       <>
-      <div>index</div>
+        <div>index</div>
       </>
     )
   }
