4cfcb35c6dd852f581f079604faa725162ae14dd
Piu-Lin <55540481+Piu-Lin@users.noreply.github.com>
2023-10-09 17:58:35 +0800

    update sub

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,10 @@ import DeliberativeManagement from './icon/DeliberativeManagement.png'
 import Questionnaires from './icon/Questionnaires.png'
 import RABList from './icon/RABList.png'
 import emergencyIcon from "./icon/emergencyIcon.png"
+import OnlineVote from './icon/OnlineVote.png'
+import ConflictMediation from './icon/ConflictMediation.png'
+import CorruptionConvention from './icon/CorruptionConvention.png'
+import CleanAndOpen from './icon/CleanAndOpen.png'
 export default class Home extends Component {
   state = {
     /** 乡村资讯 */
@@ -32,7 +36,7 @@ export default class Home extends Component {
       <>
       <div className='HomeNavBox'>
         <img id="hero" src={Hero} alt="hero" />
-        <div className='HomeNavCard'>
+        <div  style={{top:'-5vh'}} className='HomeNavCard'>
           <div id="emergencyMessage">
             <img id='emergencyIcon' src={emergencyIcon} alt="emergencyIcon" />
             <div id="emergencyContent">{this.state.emergency.content}</div>
@@ -63,7 +67,38 @@ export default class Home extends Component {
           </div>
         </div>
         <div className='HomeNavCard'>
-          
+          <div className='homeSubCardLine'>
+            <div style={{backgroundColor:'#fff6f6'}} className='homeSubCardItem homeSubCardItemReplenish'>
+              <img className='homeSubCardBgI' src={OnlineVote} alt="OnlineVote" />
+              <div>
+                <div className='homeSubCardCaption'>线上投票</div>
+                <div className='homeSubCardPart'>民主决策</div>
+              </div>
+            </div>
+            <div style={{backgroundColor:'#f1f8ff'}} className='homeSubCardItem homeSubCardItemReplenish'>
+              <img className='homeSubCardBgI' src={ConflictMediation} alt="ConflictMediation" />
+              <div>
+                <div className='homeSubCardCaption'>矛盾调解</div>
+                <div className='homeSubCardPart'>安居乐业</div>
+              </div>
+            </div>
+          </div>
+          <div className='homeSubCardLine'>
+            <div style={{backgroundColor:'#f1fdff'}} className='homeSubCardItem homeSubCardItemReplenish'>
+              <img className='homeSubCardBgI' src={CorruptionConvention} alt="CorruptionConvention" />
+              <div>
+                <div className='homeSubCardCaption'>清廉公约</div>
+                <div className='homeSubCardPart'>民众约定</div>
+              </div>
+            </div>
+            <div style={{backgroundColor:'#fff9f2'}} className='homeSubCardItem homeSubCardItemReplenish'>
+              <img className='homeSubCardBgI' src={CleanAndOpen} alt="CleanAndOpen" />
+              <div>
+                <div className='homeSubCardCaption'>清廉公开</div>
+                <div className='homeSubCardPart'>人民监督</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       </>
